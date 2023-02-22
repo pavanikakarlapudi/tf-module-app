be166541887654f24546ce03d6d18e71ce532886
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-22 22:33:47 +0530

    terr

--- locals.tf
+++ locals.tf
@@ -5,11 +5,4 @@ locals {
     business_unit = "ecommerce"
     owner         = "ecommerce-robot"
   }
-  all_tags = [
-    { key = "env", value = var.env },
-    { key = "project", value = "roboshop" },
-    { key = "business_unit", value = "ecommerce" },
-    { key = "owner", value = "ecommerce-robot" },
-    { key = "Name", value = "${var.env}-${var.component}" }
-  ]
 }
--- vars.tf
+++ vars.tf
@@ -2,10 +2,3 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "component" {}
-variable "app_port" {}
-variable "max_size" {}
-variable "min_size" {}
-variable "desired_capacity" {}
-variable "instance_type" {}
-variable "bastion_cidr" {}
