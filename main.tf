0e3f90b3dc1e96c7f589fd147078db42a367b881
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-23 17:32:15 +0530

    terr

--- main.tf
+++ main.tf
@@ -1,24 +1,88 @@
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet-group"
-  subnet_ids = var.subnet_ids
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-subnet-group" }
+    { Name = "${var.env}-${var.component}-role" }
   )
 }
 
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb-security-group"
-  description = "${var.env}-docdb-security-group"
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.component}-parameter-store-policy"
+  path        = "/"
+  description = "${var.env}-${var.component}-parameter-store-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component}*",
+          "arn:aws:ssm:us-east-1:633788536644:parameter/nexus*",
+          "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.docdb*"
+        ]
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "role-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "MongoDB"
-    from_port   = 27017
-    to_port     = 27017
+    description = "HTTP"
+    from_port   = var.app_port
+    to_port     = var.app_port
     protocol    = "tcp"
     cidr_blocks = var.allow_cidr
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.bastion_cidr
   }
 
   egress {
@@ -30,6 +94,45 @@ resource "aws_security_group" "docdb" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-security-group" }
+    { Name = "${var.env}-${var.component}-security-group" }
   )
+}
+
+resource "aws_launch_template" "main" {
+  name                   = "${var.env}-${var.component}-template"
+  image_id               = data.aws_ami.centos8.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  user_data              = base64encode(templatefile("${path.module}/user-data.sh", { component = var.component, env = var.env }))
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.profile.arn
+  }
+
+  instance_market_options {
+    market_type = "spot"
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.env}-${var.component}-asg"
+  max_size            = var.max_size
+  min_size            = var.min_size
+  desired_capacity    = var.desired_capacity
+  force_delete        = true
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  dynamic "tag" {
+    for_each = local.all_tags
+    content {
+      key                 = tag.value.key
+      value               = tag.value.value
+      propagate_at_launch = true
+    }
+  }
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,10 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
+variable "component" {}
+variable "app_port" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "desired_capacity" {}
+variable "instance_type" {}
+variable "bastion_cidr" {}
